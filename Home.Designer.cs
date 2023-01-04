5a955610595b7ad4eb6ffdb26e3427558b98e1c7
vspillai02@gmail.com <vspillai02@gmail.com>
2023-01-04 12:43:25 -0600

    created framework

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace translator
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,18 @@ namespace translator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ Home.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+var client = new RestClient("https://text-translator2.p.rapidapi.com/translate");
+var request = new RestRequest(Method.POST);
+request.AddHeader("content-type", "application/x-www-form-urlencoded");
+request.AddHeader("X-RapidAPI-Key", "113a32bba8msh01a2e5808733a20p1a28d1jsn1c402fdabd00");
+request.AddHeader("X-RapidAPI-Host", "text-translator2.p.rapidapi.com");
+request.AddParameter("application/x-www-form-urlencoded", "source_language=en&target_language=id&text=poop", ParameterType.RequestBody);
+IRestResponse response = client.Execute(request);
+
+namespace translator
+{
+
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            this.SetControls();
+        }
+
+        private void SetControls()
+        {
+            //Form
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+    }
+}
