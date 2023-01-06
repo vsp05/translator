d35c9936e3c17803c0b273ef5a824577f265bfa2
vspillai02@gmail.com <vspillai02@gmail.com>
2023-01-06 12:39:04 -0600

    implemented API

--- Home.cs
+++ Home.cs
@@ -5,23 +5,18 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-var client = new RestClient("https://text-translator2.p.rapidapi.com/translate");
-var request = new RestRequest(Method.POST);
-request.AddHeader("content-type", "application/x-www-form-urlencoded");
-request.AddHeader("X-RapidAPI-Key", "113a32bba8msh01a2e5808733a20p1a28d1jsn1c402fdabd00");
-request.AddHeader("X-RapidAPI-Host", "text-translator2.p.rapidapi.com");
-request.AddParameter("application/x-www-form-urlencoded", "source_language=en&target_language=id&text=poop", ParameterType.RequestBody);
-IRestResponse response = client.Execute(request);
 
 namespace translator
 {
 
     public partial class Home : Form
     {
+
         public Home()
         {
             InitializeComponent();
@@ -39,6 +34,33 @@ namespace translator
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            API();
+        }
+
+        private async void API()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://text-translator2.p.rapidapi.com/translate"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "113a32bba8msh01a2e5808733a20p1a28d1jsn1c402fdabd00" },
+                    { "X-RapidAPI-Host", "text-translator2.p.rapidapi.com" },
+                },
+                            Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "source_language", "en" },
+                    { "target_language", "id" },
+                    { "text", "chicken" },
+                }),
+            };
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
         }
 
     }
