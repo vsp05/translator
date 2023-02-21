013f1eec7d50947fb68c5c15a301f0881786ea3d
vspillai02@gmail.com <vspillai02@gmail.com>
2023-02-21 12:37:04 -0600

    removed API for now

--- FileTranslator.cs
+++ FileTranslator.cs
@@ -73,38 +73,6 @@ namespace translator
             //this.lstCharacters.DataSource = theguardians;
         }
 
-        private async void API(string lang1, string lang2, string text)
-        {
-
-            string inputLang = lang1;
-            string outputLang = lang2;
-            string inputText = text;
-
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new Uri("https://text-translator2.p.rapidapi.com/translate"),
-                Headers =
-                {
-                    { "X-RapidAPI-Key", "113a32bba8msh01a2e5808733a20p1a28d1jsn1c402fdabd00" },
-                    { "X-RapidAPI-Host", "text-translator2.p.rapidapi.com" },
-                },
-                Content = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    { "source_language", inputLang },
-                    { "target_language", outputLang },
-                    { "text", inputText },
-                }),
-            };
-
-            var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            string body = await response.Content.ReadAsStringAsync();
-            JObject obj = JObject.Parse(body);
-            //translatedTxt.Text = (obj["data"]["translatedText"]).ToString();
-        }
-
         private void inputLangInputTxt_TextChanged(object sender, EventArgs e)
         {
             inLang = this.inputLangInputTxt.Text;
